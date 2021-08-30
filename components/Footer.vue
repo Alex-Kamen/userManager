3c6cda855c349f67e7ca0e3520ad7233bd2b692e
NekoChan <50769901+Alex-Kamen@users.noreply.github.com>
2021-08-30 03:52:32 +0300

    Create Footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <div class="container">
+      <h3>2021 - тестовое задание</h3>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background: #4E8DE0;
+}
+
+h3 {
+  color: white;
+  font-family: Arial, sans-serif;
+  padding: 10px 0;
+  text-align: center;
+  font-weight: 400;
+  font-size: 18px;
+}
+</style>

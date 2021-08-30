5e8aa70033865a88a2d0e2182d08e635e5cc4ae6
NekoChan <50769901+Alex-Kamen@users.noreply.github.com>
2021-08-30 03:53:01 +0300

    Create user.js

---
+++ store/user.js
@@ -0,0 +1,70 @@
+export const state = {
+  userList: [],
+  userInfo: {}
+}
+
+export const mutations = {
+  setUserList(state, userList) {
+    state.userList = userList
+  },
+  setUser(state, user) {
+    state.userInfo = user
+  },
+  removeUser(state, userId) {
+    state.userList = state.userList.filter((user) => user.id != userId)
+  },
+  addUser(state, user) {
+    state.userList.push(user)
+  },
+  updateUser(state, user) {
+    state.userInfo = user
+    const userId = user.id
+    for(let i = 0; i < state.userList.length; i++) {
+      if(state.userList[i].id == userId) state.userList[i] = user
+    }
+  }
+}
+
+export const actions = {
+  fetchUserList({commit}) {
+    const userList = JSON.parse(localStorage.getItem('userList'))
+    commit('setUserList', userList)
+  },
+  fetchUser({commit}, userId) {
+    const userList = JSON.parse(localStorage.getItem('userList'))
+    for(let i = 0; i < userList.length; i++) {
+      if(userList[i].id == userId) {
+        commit('setUser', userList[i])
+        break
+      }
+    }
+  },
+  removeUser({commit, state}, userId) {
+    commit('removeUser', userId);
+    localStorage.setItem('userList', JSON.stringify(state.userList))
+  },
+  addUser({commit, state}, user) {
+    commit('addUser', user)
+    localStorage.setItem('userList', JSON.stringify(state.userList))
+  },
+  updateUser({commit, state}, user) {
+    commit('updateUser', user)
+    localStorage.setItem('userList', JSON.stringify(state.userList))
+  }
+}
+
+
+export const getters = {
+  userList(state) {
+    return state.userList
+  },
+  userInfo(state) {
+    return state.userInfo
+  },
+  maleCount(state) {
+    return state.userList.filter((user) => user.sex === 'Мужчина').length
+  },
+  femaleCount(state) {
+    return state.userList.filter((user) => user.sex === 'Женщина').length
+  }
+}

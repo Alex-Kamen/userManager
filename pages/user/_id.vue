53193d4837f8b2bb577ff0dd4e76c78a1b0c3e80
NekoChan <50769901+Alex-Kamen@users.noreply.github.com>
2021-08-30 03:52:54 +0300

    Create _id.vue

---
+++ pages/user/_id.vue
@@ -0,0 +1,109 @@
+<template>
+  <h1 v-if="!userIsSearch" class="error">Пользователь не найден</h1>
+  <div v-else>
+    <h1>Информация о пользователе</h1>
+    <div class="userData">
+      <div v-if="!userIsEdit" class="userInfo">
+        <div>
+          <span>Name: </span>
+          <span>{{userInfo.name}}</span>
+        </div>
+        <div>
+          <span>Surname: </span>
+          <span>{{userInfo.surname}}</span>
+        </div>
+        <div>
+          <span>age: </span>
+          <span>{{userInfo.age}}</span>
+        </div>
+        <div>
+          <span>Login: </span>
+          <span>{{userInfo.login}}</span>
+        </div>
+        <div>
+          <span>Password: </span>
+          <span>{{userInfo.password}}</span>
+        </div>
+        <div>
+          <span>Location: </span>
+          <span>{{userInfo.location}}</span>
+        </div>
+        <div>
+          <span>Email: </span>
+          <span>{{userInfo.email}}</span>
+        </div>
+        <div>
+          <span>Sex: </span>
+          <span>{{userInfo.sex}}</span>
+        </div>
+      </div>
+      <UpdateUser :userInfo="userInfo" @update-user="editUser" v-else/>
+      <div class="buttons">
+        <button @click.prevent="editUser" class="btn save">Редактировать</button>
+        <button @click.prevent="removeUser($route.params.id)" class="btn remove">Удалить</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import UpdateUser from "../../components/UpdateUser";
+
+export default {
+  components: {
+    UpdateUser
+  },
+  computed: {
+    userInfo() {
+      return this.$store.getters['user/userInfo']
+    }
+  },
+  mounted() {
+    this.$store.dispatch('user/fetchUser', this.$route.params.id)
+    if(!this.$store.getters['user/userInfo'].id) this.userIsSearch = false
+  },
+  data() {
+    return {
+      userIsEdit: false,
+      userIsSearch: true
+    }
+  },
+  methods: {
+    removeUser(userId) {
+      this.$store.dispatch('user/removeUser', userId)
+      this.$router.push(`/`)
+    },
+    editUser() {
+      this.userIsEdit = !this.userIsEdit
+    }
+  },
+  validate({params}) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style scoped>
+span {
+  font-size: 17px;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  font-family: Arial, sans-serif;
+  padding: 10px 0;
+}
+
+.userData {
+  display: flex;
+}
+
+.buttons, .userInfo {
+  margin-top: 10px;
+}
+
+.buttons {
+  margin-left: 60px;
+}
+</style>
